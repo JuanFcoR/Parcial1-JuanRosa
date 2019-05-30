e79afd20f843bd31f66a6931a660269b7e3b0092
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-05-29 23:42:49 -0400

    Creacion de la clase Productos

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Parcial1_JuanRosa.Entidades
 {
+    
     public class Productos
     {
+        [Key]
+        public int productoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public double Costo { get; set; }
+        public double ValorExistencia { get; set; }
+
+        public Productos()
+        {
+            productoId = 0;
+            Descripcion = String.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorExistencia = 0;
+        }
 
     }
 }
